f9a27f2ff18e3babd531649a89cc4b7cc2d30b07
gautier <gautier.blandin@gmail.com>
2022-11-20 20:31:38 +0100

    Fix tarjan function

--- graph/tarjan.py
+++ graph/tarjan.py
@@ -1,9 +1,10 @@
-def tarjan(graph: list[list[int]]) -> list[set[int]]:
+def tarjan(graph: list[list[int]], debug=False) -> list[set[int]]:
     """
     Compute the list of Strongly Connected Components (SCCs) of the graph.
 
     Args:
         graph: A directed graph represented as an adjacency list
+        debug: debug mode for logging
 
     Returns:
         The list of strongly connected components
@@ -11,6 +12,7 @@ def tarjan(graph: list[list[int]]) -> list[set[int]]:
     n = len(graph)
     visited = n * [False]
     stack = []
+    on_stack = n * [False]
     indexes = [-1] * n
     index = 0
     low_link = [float('inf')] * n
@@ -18,29 +20,51 @@ def tarjan(graph: list[list[int]]) -> list[set[int]]:
     sccs = []
 
     def dfs(node: int):
+        if debug:
+            print(f'node: {node}')
+            print(f'stack: {stack}')
+            print(f'visited: {visited}')
+            print(f'indexes: {indexes}')
+            print(f'low_link: {low_link}')
+
         nonlocal index
         indexes[node] = index
+        low_link[node] = index
         index += 1
 
         stack.append(node)
+        on_stack[node] = True
 
         for child in graph[node]:
             if not visited[child]:
                 visited[child] = True
                 dfs(child)
-                low_link[node] = min(low_link[node], low_link[child], indexes[child])
+                low_link[node] = min(low_link[node], low_link[child])
             else:
-                if stack.__contains__(child):
+                if on_stack[child]:
                     low_link[node] = min(low_link[node], indexes[child])
 
-        if low_link[node] <= indexes[node]:
+        if low_link[node] == indexes[node]:
             scc = set()
-            while s := stack.pop != node:
+            s = stack.pop()
+            while True:
                 scc.add(s)
+                on_stack[s] = False
+
+                if s == node:
+                    break
+                s = stack.pop()
+
             sccs.append(scc)
+            if debug:
+                print(f'new scc: {scc}')
+                print(f'sccs: {sccs}')
 
     for start in range(n):
         if not visited[start]:
+            visited[start] = True
             dfs(start)
 
+    if debug:
+        print(f'sccs: {sccs}')
     return sccs
