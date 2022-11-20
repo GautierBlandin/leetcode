d6ad876c4e932b60e2eecd583877df3a5c09df12
gautier <gautier.blandin@gmail.com>
2022-11-20 19:32:26 +0100

    Add binary searcher tests

--- test/medium/test_longest_substring_no_repeat.py
+++ test/medium/test_longest_substring_no_repeat.py
@@ -1,6 +1,7 @@
 from typing import Callable
 
 import medium.longest_substring_no_repeat as longest_substring_no_repeat
+from medium.longest_substring_no_repeat import BinarySearcher
 
 
 def longest_substring_no_repeat_tester(longest_substring_no_repeat_callable: Callable[[str], int], verbose=0):
@@ -29,3 +30,27 @@ def test_generate_all_substring_of_length_n():
     assert list(longest_substring_no_repeat.generate_all_substring_of_length_n(" ", 1)) == [" "]
     assert list(longest_substring_no_repeat.generate_all_substring_of_length_n("abcd", 1)) == ["a", "b", "c", "d"]
     assert list(longest_substring_no_repeat.generate_all_substring_of_length_n("abcd", 3)) == ["abc", "bcd"]
+
+
+def test_binary_searcher():
+    bs = BinarySearcher(5, 15)
+    assert bs.get_value() == 10
+    bs.lower()
+    assert bs.get_value() == 8
+    bs.higher()
+    assert bs.get_value() == 9
+    bs.higher()
+    assert bs.get_value() == 10
+
+    bs = BinarySearcher(5, 15)
+    assert bs.get_value() == 10
+    bs.lower()
+    assert bs.get_value() == 8
+    bs.lower()
+    assert bs.get_value() == 7
+    bs.lower()
+    assert bs.get_value() == 6
+    bs.lower()
+    assert bs.get_value() == 5
+
+
