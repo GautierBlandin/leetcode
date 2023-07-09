f7859431264b883f7c97985ecda828fdadddbf33
gauti <gautier.blandin@gmail.com>
2023-07-09 17:58:39 +0200

    Encapsulate comparison and increment

--- easy/merge_two_sorted_lists.py
+++ easy/merge_two_sorted_lists.py
@@ -1,23 +1,30 @@
 class SortedListMerger:
-    def merge(self, list1, list2):
+    def merge(self, list_i, list_j):
         result = []
-        list1_index = 0
-        list2_index = 0
-        while not self.lists_fully_iterated(list1_index, list2_index, list1, list2):
-            if list1_index < len(list1) and list2_index < len(list2):
-                if list1[list1_index] < list2[list2_index]:
-                    result.append(list1[list1_index])
-                    list1_index += 1
-                else:
-                    result.append(list2[list2_index])
-                    list2_index += 1
-            elif list1_index < len(list1):
-                result.append(list1[list1_index])
-                list1_index += 1
-            elif list2_index < len(list2):
-                result.append(list2[list2_index])
-                list2_index += 1
+        i = 0
+        j = 0
+        while not self.lists_fully_iterated(i, j, list_i, list_j):
+            if self.both_lists_not_finished(i, j, list_i, list_j):
+                i, j = self.append_minimum_and_increment_index(i, j, list_i, list_j, result)
+            elif i < len(list_i):
+                result.append(list_i[i])
+                i += 1
+            elif j < len(list_j):
+                result.append(list_j[j])
+                j += 1
         return result
 
     def lists_fully_iterated(self, i, j, list_i, list_j):
         return i >= len(list_i) and j >= len(list_j)
+
+    def both_lists_not_finished(self, i, j, list_i, list_j):
+        return i < len(list_i) and j < len(list_j)
+
+    def append_minimum_and_increment_index(self, i, j, list_i, list_j, result):
+        if list_i[i] < list_j[j]:
+            result.append(list_i[i])
+            i += 1
+        else:
+            result.append(list_j[j])
+            j += 1
+        return i, j
