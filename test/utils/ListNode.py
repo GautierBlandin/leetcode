38aa19638a1421beac28bdbef4bfbdb720d66866
gauti <gautier.blandin@gmail.com>
2023-07-14 09:44:55 +0200

    Add from_list to ListNode

--- test/utils/ListNode.py
+++ test/utils/ListNode.py
@@ -5,3 +5,18 @@ def test_list_node_to_array():
     head = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5)))))
     assert list_nodes_to_array(head) == [1, 2, 3, 4, 5]
     assert list_nodes_to_array(None) == []
+
+def test_from_list():
+    head = ListNode.from_list([1, 2, 3, 4])
+    assert head.val == 1
+    assert head.next.val == 2
+    assert head.next.next.val == 3
+    assert head.next.next.next.val == 4
+    assert head.next.next.next.next is None
+
+    head = ListNode.from_list([])
+    assert head is None
+
+    head = ListNode.from_list([0])
+    assert head.val == 0
+    assert head.next is None
--- utils/ListNode.py
+++ utils/ListNode.py
@@ -3,6 +3,33 @@ class ListNode(object):
         self.val = val
         self.next = next
 
+    @staticmethod
+    def from_list(l):
+        if len(l) == 0:
+            return None
+
+        head = ListNode(l[0])
+        current = head
+
+        for i in range(1, len(l)):
+            next_node = ListNode(l[i], None)
+            current.next = next_node
+            current = next_node
+
+        return head
+
+    @staticmethod
+    def to_list(head):
+        if head is None:
+            return []
+
+        result = [head.val]
+        current = head
+        while current.next is not None:
+            current = current.next
+            result.append(current.val)
+        return result
+
 
 def list_nodes_to_array(head: ListNode | None):
     if head is None:
