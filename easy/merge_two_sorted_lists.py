ccc95e138c9bab87afa44ecc6018ac6513091d36
gauti <gautier.blandin@gmail.com>
2023-07-14 09:45:10 +0200

    Fix merge_two_sorted_lists.py to use LinkedLists

--- easy/merge_two_sorted_lists.py
+++ easy/merge_two_sorted_lists.py
@@ -1,42 +1,60 @@
+from utils.ListNode import ListNode
+
+
 class SortedListMerger:
     def __init__(self):
-        self.i = 0
-        self.j = 0
-        self.list_i = []
-        self.list_j = []
-        self.result = []
+        self.current_i = None
+        self.current_j = None
+        self.head_i = None
+        self.head_j = None
+        self.result_head = None
+        self.result_current = None
 
-    def merge(self, list_i, list_j):
-        self.i = 0
-        self.j = 0
-        self.list_i = list_i
-        self.list_j = list_j
-        self.result = []
+    def merge(self, head_i, head_j):
+        self.init_merge(head_i, head_j)
+
         while not self.lists_fully_iterated():
             if self.both_lists_active():
-                self.append_minimum_and_increment_index()
+                self.append_minimum_and_increment()
             else:
                 self.append_remaining_items()
-        return self.result
+        return self.result_head
+
+    def init_merge(self, head_i, head_j):
+        self.current_i = head_i
+        self.current_j = head_j
+        self.head_i = head_i
+        self.head_j = head_j
+        self.result_head = None
+        self.result_current = None
 
     def lists_fully_iterated(self):
-        return self.i >= len(self.list_i) and self.j >= len(self.list_j)
+        return self.current_i is None and self.current_j is None
 
     def both_lists_active(self):
-        return self.i < len(self.list_i) and self.j < len(self.list_j)
+        return self.current_i is not None and self.current_j is not None
 
-    def append_minimum_and_increment_index(self):
-        if self.list_i[self.i] < self.list_j[self.j]:
-            self.result.append(self.list_i[self.i])
-            self.i += 1
+    def append_to_result(self, node):
+        if self.result_head is None:
+            self.result_head = ListNode(node.val)
+            self.result_current = self.result_head
         else:
-            self.result.append(self.list_j[self.j])
-            self.j += 1
+            new_node = ListNode(node.val)
+            self.result_current.next = new_node
+            self.result_current = new_node
+
+    def append_minimum_and_increment(self):
+        if self.current_i.val < self.current_j.val:
+            self.append_to_result(self.current_i)
+            self.current_i = self.current_i.next
+        else:
+            self.append_to_result(self.current_j)
+            self.current_j = self.current_j.next
 
     def  append_remaining_items(self):
-        if self.i < len(self.list_i):
-            self.result.extend(self.list_i[self.i:])
-            self.i = len(self.list_i)
-        elif self.j < len(self.list_j):
-            self.result.extend(self.list_j[self.j:])
-            self.j = len(self.list_j)
+        while self.current_i is not None:
+            self.append_to_result(self.current_i)
+            self.current_i = self.current_i.next
+        while self.current_j is not None:
+            self.append_to_result(self.current_j)
+            self.current_j = self.current_j.next
--- test/easy/test_merge_two_sorted_lists.py
+++ test/easy/test_merge_two_sorted_lists.py
@@ -1,10 +1,15 @@
 from easy.merge_two_sorted_lists import SortedListMerger
+from utils.ListNode import ListNode
 
 
 def test_merge_two_sorted_lists():
+    assert merge_list_helper([], []) == []
+    assert merge_list_helper([1, 3, 5], [2, 4, 6]) == [1, 2, 3, 4, 5, 6]
+    assert merge_list_helper([1, 3, 7, 9], [2, 4]) == [1, 2, 3, 4, 7, 9]
+    assert merge_list_helper([1, 2, 3], []) == [1, 2, 3]
+    assert merge_list_helper([], [1, 2, 3]) == [1, 2, 3]
+
+
+def merge_list_helper(list_1, list_2):
     merger = SortedListMerger()
-    assert merger.merge([], []) == []
-    assert merger.merge([1, 3, 5], [2, 4, 6]) == [1, 2, 3, 4, 5, 6]
-    assert merger.merge([1, 3, 7, 9], [2, 4]) == [1, 2, 3, 4, 7, 9]
-    assert merger.merge([1, 2, 3], []) == [1, 2, 3]
-    assert merger.merge([], [1, 2, 3]) == [1, 2, 3]
+    return ListNode.to_list(merger.merge(ListNode.from_list(list_1), ListNode.from_list(list_2)))
